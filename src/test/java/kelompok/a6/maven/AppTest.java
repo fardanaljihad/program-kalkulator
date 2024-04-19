3b305829428b87a411bdc09c0dfa7ffd8d284c4d
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-04-19 07:32:48 +0700

    add Unit Tesing on modul Calculate

--- src/test/java/kelompok/a6/maven/AppTest.java
+++ src/test/java/kelompok/a6/maven/AppTest.java
@@ -1,20 +1,42 @@
 package kelompok.a6.maven;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+    //Test for module Calculate
+    //Test for addition
+    @Test
+    public void testCalculateAddition() {
+        assertEquals(11, App.calculate(10, 1, '+'));
+    }
+
+    //Test for subtraction
+    @Test
+    public void testCalculateSubtraction() {
+        assertEquals(11, App.calculate(21, 10, '-'));
+    }
+
+    //Test for multiplication
+    @Test
+    public void testCalculateMultiplication() {
+        assertEquals(14, App.calculate(7, 2, '*'));
+    }
+
+    //Test for division
+    @Test
+    public void testCalculateDivision() {
+        assertEquals(2, App.calculate(8, 4, '/'));
+    }
+
 }
